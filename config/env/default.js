fb78462e468b26107d6f631d552819ffef51af21
jayagl <jagrawal@isw.net.au>
2017-06-25 13:23:37 +1000

    UI

--- config/env/default.js
+++ config/env/default.js
@@ -28,7 +28,7 @@ module.exports = {
   sessionKey: 'sessionId',
   sessionCollection: 'sessions',
   logo: 'modules/core/client/img/brand/logo.png',
-  favicon: 'modules/core/client/img/brand/favicon.ico',
+  favicon: 'modules/core/client/img/brand/favicon.png',
   uploads: {
     profileUpload: {
       dest: './modules/users/client/img/profile/uploads/', // Profile upload destination path
