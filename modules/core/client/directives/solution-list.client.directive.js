9a7cbeaf832f401eba0be3aab0a517cff3d065cb
Rohan Richards <rohan.m.richards@gmail.com>
2017-11-22 18:12:33 +1000

    solution-list implemented

---
+++ modules/core/client/directives/solution-list.client.directive.js
@@ -0,0 +1,89 @@
+'use strict';
+
+angular.module('core').directive('solutionList', ['$timeout', function ($timeout) {
+	return {
+		restrict: 'E',
+		scope: {
+			solutions: '=',
+			goalId: '='
+		},
+		templateUrl: 'modules/core/client/views/solutions-list.client.view.html',
+		bindToController: true,
+		controllerAs: 'vm',
+		controller: ['$scope', '$window', 'VoteService', 'SortService', 'Authentication', 'SocialshareService', 'RegionService', 'GoalService', 'SolutionService',
+			function ($scope, $window, VoteService, SortService, Authentication, SocialshareService, RegionService, GoalService, SolutionService) {
+				var vm = this;
+				vm.sortSvc = SortService;
+				vm.regions = [];
+				$scope.authentication = Authentication;
+
+				console.log('solution-list - have solutions: ', vm.solutions);
+
+				vm.vote = function (solution, voteType, $event) {
+					$event.stopPropagation();
+					VoteService.vote(solution, 'solution', voteType).then(function (data) {
+						solution.$get();
+					});
+				};
+				vm.sort = function (sortData, $event) {
+					if ($event) $event.stopPropagation();
+					SortService.setSort('solution', sortData.type, sortData.order);
+				};
+
+				vm.share = function (solution, provider) {
+					SocialshareService.share({
+						provider: provider,
+						rel_url: '/solutions/' + solution._id,
+						title: solution.title,
+						hashtags: solution.tags.join()
+					});
+				};
+
+				vm.chartLabels = ['Against', 'For'];
+				vm.chartOptions = {
+					elements: {
+						arc: {
+							borderWidth: 0
+						}
+					},
+					responsive: true,
+					legend: {
+						display: false
+					}
+				};
+
+				vm.searchRegions = function (query) {
+					return RegionService.searchRegions(query);
+				};
+
+				vm.updateVotes = function (regions) {
+					SolutionService.list({
+						goalId: vm.goalId ? vm.goalId : null,
+						issueId: vm.issueId ? vm.issueId: null,
+						regions: regions
+					}).then(function (solutions) {
+						console.log(solutions);
+						vm.solutions = solutions;
+					});
+				};
+
+				vm.chartColors = [{
+					backgroundColor: 'rgba(255,0,0,0.8)',
+					pointBackgroundColor: 'rgba(255,0,0,0.5)',
+					pointHoverBackgroundColor: 'rgba(255,0,0,0.6)',
+					borderColor: 'rgba(255,0,0,0.6)',
+					pointBorderColor: 'rgba(255,0,0,0.6)',
+					pointHoverBorderColor: 'rgba(255,0,0,0.6)'
+				},
+				{
+					backgroundColor: 'rgba(0,255,0,0.8)',
+					pointBackgroundColor: 'rgba(0,255,0,0.5)',
+					pointHoverBackgroundColor: 'rgba(77,83,96,1)',
+					borderColor: 'rgba(77,83,96,1)',
+					pointBorderColor: '#fff',
+					pointHoverBorderColor: 'rgba(77,83,96,0.8)'
+				}];
+			}
+		]
+	};
+}]);
